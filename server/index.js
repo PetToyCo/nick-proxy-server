3d227eefaeb2520b4669242c56d2bdabbc2db6bb
Nick-Zabalza <nickyz2565@gmail.com>
2020-06-29 19:58:59 -0700

    checking again

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ server.use(morgan('dev'));
 server.use(cookieParser());
 
 server.get('/', (req, res, next) => {
-  const { PTCItemID, TempItemID} = req.cookies
+  const { PTCItemID, TempItemID } = req.cookies;
   if (PTCItemID && TempItemID) {
     res.clearCookie('TempItemID');
     next();
@@ -22,8 +22,8 @@ server.use(serveStatic('client'));
 
 server.get('/product', (req, res) => {
   const { itemID } = req.query;
-  const itemIdNumber = Number.parseInt(itemID);
-  const { TempItemID } = req.cookies
+  const itemIdNumber = Number.parseInt(itemID, 10);
+  const { TempItemID } = req.cookies;
 
   if (itemIdNumber < 100 || itemIdNumber > 199 || itemIdNumber === undefined) {
     res.status(404).send();
@@ -31,6 +31,5 @@ server.get('/product', (req, res) => {
     res.cookie('TempItemID', itemID).cookie('PTCItemID', itemID).redirect('/');
   }
 });
-
 
 server.listen(3000);
