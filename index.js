b3003ff2ff596a848738c7d2fbb0345a4315bac4
Nick-Zabalza <nickyz2565@gmail.com>
2020-06-29 21:19:44 -0700

    updated files to use search params rather than cookies

--- index.js
+++ index.js
@@ -8,27 +8,16 @@ const server = express();
 server.use(morgan('dev'));
 server.use(cookieParser());
 
-server.get('/', (req, res, next) => {
-  const { PTCItemID, TempItemID } = req.cookies;
-  if (PTCItemID && TempItemID) {
-    res.clearCookie('TempItemID');
-    next();
-  } else {
-    res.status(404).send();
-  }
-});
-
 server.use(serveStatic('client'));
 
-server.get('/product', (req, res) => {
+server.get('/*', (req, res) => {
   const { itemID } = req.query;
   const itemIdNumber = Number.parseInt(itemID, 10);
-  const { TempItemID } = req.cookies;
 
   if (itemIdNumber < 100 || itemIdNumber > 199 || itemIdNumber === undefined) {
     res.status(404).send();
-  } else if (TempItemID === undefined) {
-    res.cookie('TempItemID', itemID).cookie('PTCItemID', itemID).redirect('/');
+  } else {
+    res.sendFile(`${__dirname}/client/index.html`);
   }
 });
 
